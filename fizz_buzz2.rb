2693b9f2281e0af0878a9f0c8f39699d83944059
Brittny Ferrier <bdferrier@gmail.com>
2016-11-08 01:09:00 +0000

    Get my new project

---
+++ fizz_buzz2.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(x)
+    if ( x % 3 == 0 ) && ( x % 5 ==0 )
+        puts "#{x}. FizzBuzz!"
+    elsif x % 3 == 0
+        puts "#{x}. Fizz!"
+    elsif x % 5 == 0
+        puts "#{x}. Buzz!"
+    
+    else
+        puts "#{x}."
+    
+    end
+end
+
+puts "Hi... what number should I test?"
+num = gets.chomp.to_i
+
+fizzbuzz(num)
